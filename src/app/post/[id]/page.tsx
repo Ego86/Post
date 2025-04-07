8d59fe56475b0ea3bee9b41fa382ae8f23945456
Ego86 <endeerfed215@mail.ru>
2025-04-07 14:06:43 +0500

    upd post

--- src/app/post/[id]/page.tsx
+++ src/app/post/[id]/page.tsx
@@ -5,17 +5,7 @@ import styles from "./page.module.scss";
 import { IPostComments } from "@/shared/types/interface";
 import Link from "next/link";
 import Header from "@/widgets/header";
-// import { NextSeo } from "next-seo";
-//Invalid hook call. Hooks can only be called inside of the body of a function component. This could happen for one of the following reasons:
-// 1. You might have mismatching versions of React and the renderer (such as React DOM)
-// 2. You might be breaking the Rules of Hooks
-// 3. You might have more than one copy of React in the same app
-// See https://react.dev/link/invalid-hook-call for tips about how to debug and fix this problem.
-//  ⨯ [TypeError: Cannot read properties of null (reading 'useContext')] {
-//   digest: '1776150244'
-// }
-//
-//
+import { ArticleJsonLd, ProductJsonLd } from "next-seo";
 
 const comments: IPostComments[] = [
   {
@@ -39,9 +29,19 @@ const Post = async ({ params }: { params: Promise<Params> }) => {
   const post = await postApi.getPostById(Number(id));
   // const comments = await postApi.getPostComments(Number(id));
   // при 2 запросах сервер jsonplaceholder не выдает данные
+  const date = JSON.stringify(new Date().getUTCDate())
   return (
     <>
-      {/* <NextSeo title={post.title} description={post.body}/> */}
+      <ArticleJsonLd
+        url={`http://localhost:3000/post/${post.id}`}
+        authorName={post.title}
+        useAppDir={true}
+        title={post.title}
+        description={post.body}
+        productName={post.title}
+        images={[]}
+        datePublished={date}
+      />
       <Header />
       <main className={styles.main}>
         <Link className={styles.back} href="/1">
